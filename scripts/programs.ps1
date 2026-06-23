dc4d2e76dada05ec784354c2752665142775f0a0
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-06-23 14:12:05 -0400

    chore(vim): aclarartion comment on  function

--- scripts/programs.ps1
+++ scripts/programs.ps1
@@ -1,3 +1,3 @@
 function vim {
-    & "C:\Users\Manuel Morales\AppData\Local\Programs\Git\usr\bin\vim.exe" $args
+    & "C:\Users\Manuel Morales\AppData\Local\Programs\Git\usr\bin\vim.exe" $args # Change this path to the actual location of your Vim executable if different
 }
